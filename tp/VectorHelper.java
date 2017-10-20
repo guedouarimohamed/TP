8d717b8380a8f9ebd8ad6e922dc5cb1664e126c2
guedouarimohamed <mohamadovski999@gmail.com>
2017-10-20 16:37:39 +0200

    version0

---
+++ tp/VectorHelper.java
@@ -0,0 +1,42 @@
+package tp;
+
+/**
+ * Date : 22 / 10 / 2017
+ * @author Guedouari_Chouia
+ * @version final
+ * 
+ * C'est la class VrctorHelper pour manipuler les vecteurs
+ */
+public class VectorHelper {
+	 /**
+		 * C'est un vecteur des entiers
+		 */
+		private int[] Vector;
+		 
+		 /**
+		 * n indique la longueur du vecteur (le nombre des element)
+		 */
+		private int n;
+		 
+		 /**
+		 * true si le tableau est initialise ,false sinon
+		 */
+		private boolean is_initialiser = false;
+		
+		 /**
+		 * la valeur maximale du vecteur
+		 */
+		private int max;
+		 /**
+		 * la valeur minimal du vecteur
+		 */
+		private int min;
+		 
+		 /**
+		 * c'est le constructeur (sans parametre)
+		 */
+		public VectorHelper() 
+	     {
+	      
+	     }
+}
