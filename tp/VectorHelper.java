0c734bace50cb38acdbdeab2e4f83e5cd079c928
nounou4madrid <en_chouia@esi.dz>
2017-10-20 16:58:19 +0100

    version amelioré 1

--- tp/VectorHelper.java
+++ tp/VectorHelper.java
@@ -37,6 +37,87 @@ public class VectorHelper {
 		 */
 		public VectorHelper() 
 	     {
-	      
+	       
+	     }
+		 /**
+		  * C'est pour connaitre le nombre des element
+		 * @return retourner n( la taolle du vecteur)
+		 */
+		public int get_n()
+	     {
+	         return n;
+	     }
+		 
+		 /**
+		  * pour affecter la longueur du vecteur dans n
+		 * @param nb la valeur a affecté dans n
+		 */
+		public void set_n(int nb)
+	     {
+	         n = nb;
+	     }
+		 
+		 /**
+		  * pour savoir si le vecteur est initilisé
+		 * @return retourner true si le vecteur est initilisé,false sinon
+		 */
+		public boolean get_is_initialiser() 
+	     {
+	         return is_initialiser;
+	     }
+		 /**
+		  * pour savoir la valeur maximal
+		 * @return retourner le max
+		 */
+		public int get_max(){return max;}
+		
+		 /**
+		  * pour affecter la valeur maximal dans l'attribut max
+		 * @param max la valeur a affecté dans max
+		 */
+		public void set(int max){this.max=max;}
+		 
+		 /**
+		  * pour savoir la valeur minimal
+		 * @return retourner le min
+		 */
+		public int get_min(){return min;}
+		 /**
+		  * affecter la valeur minimal dans l'attribut min
+		 * @param min la valeur a affecté dans min
+		 */
+		public void set_min(int min){this.min=max;}
+		 
+		 /**
+		 * la methode qui affiche le vecteur
+		 */
+		public void affiche_Vector()
+	     {
+	         System.out.print("les elelment du vecteur sont :");
+	         for (int i = 0; i < n; i++)
+	         {
+	        	 System.out.print("   " + Vector[i]);
+	         }
+
+	         System.out.println();
+	     }
+		 
+		 /**
+		  * l'initialisation du vecteur  
+		 * @param tab un qui vecteur qui contient les valeur a affecté dans le vecteur 
+		 * @param n la taille du vecteur
+		 */
+		public void init_Vector(int tab[],int n)
+	     {
+			 this.n=n;
+	         Vector = new int[n];
+	         
+	         for (int i = 0; i < n; i++)
+	         {
+
+	             Vector[i] = tab[i];
+	         }
+	         is_initialiser = true;
 	     }
 }
+
