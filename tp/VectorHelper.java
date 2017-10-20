4e38a17a7d00f883a42593336b690f2e30a6b90a
guedouarimohamed <mohamadovski999@gmail.com>
2017-10-20 18:00:33 +0200

    version0.2

--- tp/VectorHelper.java
+++ tp/VectorHelper.java
@@ -1,5 +1,7 @@
 package tp;
 
+import TP.VectorHelper;
+
 /**
  * Date : 22 / 10 / 2017
  * @author Guedouari_Chouia
@@ -47,4 +49,62 @@ public class VectorHelper {
 	     {
 	         return Vector;
 	     }
+		
+		/**
+		 * une methode qui trie les elements du vecteur d'une maniere croissante
+		 */
+		public void tri_selection()
+	     {int c;
+	         if(is_initialiser==true)
+	         {
+	             for (int i = 0; i < n - 1; i++)
+	             {
+	               for (int j = i + 1; j < n; j++)
+	               {
+	                 if (Vector[j] < Vector[i])
+	                 {
+	                     c=Vector[i];
+	                     Vector[i]=Vector[j];
+	                     Vector[j]=c;
+	                 }
+	               }
+	             }
+	         }
+	         else
+	         {
+	        	 System.out.println("le Vector n'est pas initialiser");
+	         } 
+	     }
+		 
+		 
+		 /**
+		  * addition de deux vecteurs
+		 * @param V2 le vecteur a additioné avec V1
+		 * @throws Exception une exception se genere si les vecteurs ont des tailles differentes
+		 */
+		public void add(VectorHelper V2) throws Exception
+	     {
+	         if (is_initialiser == true && V2.get_is_initialiser() == true)
+	         {
+	             if (n == V2.get_n())
+	             {
+
+	                 for (int i = 0; i < n; i++)
+	                 {
+	                     Vector[i] = Vector[i] + V2.get_Vector()[i];
+	                 }
+
+	             }
+	             else
+	             {
+	                 throw new Exception("les longueurs de vecteurs ne sont pas egaux");
+
+	             }
+	         }
+	         else
+	         {
+	        	 System.out.println("assurez que les deux vectors sont inisialisé");
+	         }
+	     }
+		 
 }
